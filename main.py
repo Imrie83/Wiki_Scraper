3281c8acea569760810eb13b7c6c69914b20298f
Imrie83 <czolgista83@gmail.com>
2021-10-16 18:06:19 +0200

    end of day commit

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 
+
 def extract_list(url, xpath):
     options = Options()
     options.add_argument("--headless")
-    options.add_argument("--lang=it");
+    options.add_argument("--lang=it")
     driver = webdriver.Chrome('/home/imrie/Downloads/chromedriver', options=options)
     driver.get(url)
     table = []
@@ -15,8 +16,9 @@ def extract_list(url, xpath):
     driver.close()
     return table
 
+
 pages = ['Comunit%C3%A0_ebraiche_italiane', 'Cimiteri_ebraici_in_Italia',
-         'Musei_ebraici_in_Italia','Ghetti_ebraici_in_Italia','Sinagoghe_in_Italia']
+         'Musei_ebraici_in_Italia', 'Ghetti_ebraici_in_Italia', 'Sinagoghe_in_Italia']
 
 df_dict = {}
 xpath = '//*[@id="mw-content-text"]'
@@ -35,3 +37,32 @@ for i in range(1,len(pages)):
     df = pd.concat([df, df_dict[pages[i]]])
 
 df.to_csv('data/raw_data.csv')
+
+df = pd.read_csv('data/raw_data.csv')
+index_na = 83
+
+
+def is_active(x, index_na):
+    if x < index_na:
+        return True
+    return False
+
+
+df['is_active'] = df['Unnamed: 0'].apply(lambda x: is_active(x, index_na))
+bag_words = [
+    'Comunità ebraica di', '(Provinc', '(Region', ' Provinc', 'ex circondario', 'Cimitero ebraico di',
+    'Museo ebraico di', 'Ghetto di', 'Sinagoga di', 'Cimitero israelitico di',
+    'Cimitero monumentale ebraico di'
+]
+
+
+def is_locality(x):
+    for bag in bag_words:
+        if bag in x:
+            return True
+    return False
+
+
+df['is_locality'] = df['value'].apply(lambda x : is_locality(x))
+df = df[df['is_locality'] == True]
+df.reset_index(inplace = True)
